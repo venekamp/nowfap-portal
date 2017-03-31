26ccc848f3960d05ea61ce3d840b9199f4b937b1
Gerben Venekamp <venekamp@gmail.com>
2017-03-31 15:03:52 +0200

    Remove the UUID from the metadata
    The uuid was added to the metadata in order to grantee a unique
    entityID. Normally, a URL is used as that should be unique. In case the
    COmanage portal is installed on a private VM, i.e. on a user's local
    machine, the URL might not be unique and hence the original inclusion of
    the uuid. This feature is unwanted on the develop or master branch and
    hence the deletion.

--- scripts/mellon_create_metadata.sh
+++ scripts/mellon_create_metadata.sh
@@ -156,13 +156,11 @@ fi
 
 CERT="$(grep -v '^-----' "$OUTFILE.cert")"
 
-UUID=$(uuidgen)
-
 cat >"$OUTFILE.xml" <<EOF
 <?xml version="1.0"?>
 <md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata"
                      xmlns:mdui="urn:oasis:names:tc:SAML:metadata:ui"
-                     entityID="https://192.168.50.4/registry/auth/sp/metadata/$UUID">
+                     entityID="https://192.168.50.4/registry/auth/sp/metadata">
   <md:SPSSODescriptor AuthnRequestsSigned="true"
                       WantAssertionsSigned="true"
                       protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol">
