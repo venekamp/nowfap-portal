489de4ea32ce25df22bfea0d005509e5dc922277
Gerben Venekamp <venekamp@gmail.com>
2017-04-11 21:56:09 +0200

    Remove commented line

--- roles/sshd/files/get-authorized-keys.sh
+++ roles/sshd/files/get-authorized-keys.sh
@@ -6,8 +6,6 @@ log() {
     echo "$(date '+%b %d %H:%M:%S') $1" >> $LOG_FILE
 }
 
-#echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDJeoI8QvBFw11XBEosvtcZPWUJBM7l4bstXI93wZU1mLg6NcfH0CeJKIW/rsB/R3xpsD5h2eUTo7W0IG3MY4fuKG0ABIE/M1h9vomPbBD53riA+Kmseje7022N2fzDGsi6+qm2FeGIc+hHjp1fnSNbFOo1X2d7DFEoQwQn6nFJhttwLr3jJ7Afp/jtKfg7d2QF0s8IiLlBJlfplLImLGV6RXxtpuz1W1SVDLFbgtxcAwpoSzx5FHPReFafz0lylZAC6swrE4cv0PBDfaNSLRQ/RU+g73IDl6uSq/ntRpI+7CgwspAVVyf/m0fCqPms2JzOMbME3fotywrZIk/XR/lZ gerben@mlt0067.wp.surfsara.nl"
-
 LDAP_SERVER=ldap://172.29.191.100
 LDAP_ADMIN='cn=admin,dc=portal,dc=example,dc=org'
 USER_DN='mail=venekamp@gmail.com,ou=people,dc=portal,dc=example,dc=org'
